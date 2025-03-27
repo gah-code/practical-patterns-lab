685180ffdbc75bad985e993ed62b1fe29b408424
Gilbert Haro <gilbertharocode@gmail.com>
2025-03-27 03:29:04 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import './App.css';
+import TipApp from './TipApp';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>test</h1>
+      <TipApp />
+    </>
+  );
+}
+
+<h1></h1>;
+
+export default App;
